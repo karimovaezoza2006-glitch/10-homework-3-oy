a9e46deb457461c56c0876b46dfd1394fba1c187
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-09-24 23:49:46 +0500

    figma and text chala

---
+++ main.js
@@ -0,0 +1,149 @@
+// const dasturchi ={
+//     ism:"ezoza",
+//     yosh: 19,
+   
+// }
+// console.log(Object.entries(dasturchi));
+
+// let arr_number = [1, 2, 3, 4, 5, 6, 7, 8];
+// let arr = [true, "string", 22, {name:"ezoza"}, false];
+
+// arr.push("hello");
+// arr.pop();
+// arr.shift();
+// arr.shift();
+// arr.unshift("ezoza");
+// console.log(arr);
+// arr_number.splice(0,2);
+// console.log(arr_number);
+// let new_arr = arr.slice(0,3);
+// console.log(new_arr);
+// let newarr = arr.concat(arr_number);
+// console.log(newarr);
+// let stringArr =arr.join(" salom ");
+// console.log(stringArr);
+// console.log(arr_number.reverse());
+// let person_arr =[
+//     {
+//         id:1,
+//         name:"olim", 
+//         surname:"Olimov", 
+//         job:"Dehqon",
+//         price:3000000
+//     }, 
+//     {
+//        id:2,
+//         name:"olima", 
+//         surname:"Olimova", 
+//         job:"Tikuvchi",
+//         price:2500000 
+//     },
+//      {
+//        id:3,
+//         name:"olima", 
+//         surname:"Olimova", 
+//         job:"Tikuvchi",
+//         price:3500000 
+//     },
+//     {
+//        id:4,
+//         name:"ezoza", 
+//         surname:"Abdujalolova", 
+//         job:"alochi",
+//         price:3000000 
+//     }
+// ]
+// arr.forEach((value, idx, arr) => {
+//   console.log(value, idx, arr);
+// });
+
+// const dasturchi = {
+//   ism:"ulugbek",
+//   yosh:24,
+//   salomlashish:function(){
+//     console.log("mening ismim "+  this.ism);
+//   }
+// }
+// dasturchi.salomlashish();
+
+// const number =-8;
+// if(number>0){
+//   console.log("berilgan son musbat");
+// }else if (number === 0){
+// console.log("berilgan son 0");
+// }else{
+//   console.log("berilgan son manfiy");
+// }
+
+
+
+// const number =2 ;
+// let output;
+// switch (number){
+//   case 1:
+//     output = "bir";
+//     break;
+//     case 2:
+//       output = "ikki";
+//       break;
+//       default:
+//         output = "topilmadi"
+//         break;
+
+// }
+// console.log(`qiymat:${output}`);
+
+// let sum =0;
+// for ( let i=1; i<=10; i++){
+//   sum += i;
+  
+// }
+// console.log(`summa:${sum}`);
+
+// for ( let i=1; i<=5; i++){
+//   if (i==3){
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let i =0;
+// do{
+//   console.log("salom dunyo");
+//   i++;
+// }while (i<10)
+
+
+
+//   let full_name ="Usmonov Usmon";
+// let person={
+//     full_name,
+//     username:"usmon1",
+//     location:{
+//         city:"Toshkent",
+//         country:"Uzbekistan"
+//     },
+//     isMarried:false,
+//     age:18,
+//     job:"sales assistant",
+//     great:function(){
+//         console.log("salom "+ this.full_name);
+//     },
+// };
+// person.great();
+// console.log(person.age);
+// console.log(person.location.city);
+// console.log(person["isMarried"]);
+// console.log(person ["location"] ["city"]);
+
+// person.work_location="sergile 12kv";
+
+let monkeyCount =1;
+function monkeyCount (i){
+  for (i=0; i<monkeyCount.length; i++){
+    console.log(monkeyCount)
+  }
+
+
+}
+monkeyCount(20);
